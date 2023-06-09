78da5122b04471679bc85b9d6160e919848b1735
omkar <pendkalkar.omkar786@gmail.com>
2023-06-09 16:15:16 +0530

    ProductDetailsActivity added

---
+++ app/src/main/java/com/example/ecommerce/ProductDetailsActivity.java
@@ -0,0 +1,63 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Objects;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+    private static final String TAG = "ProductDetailsActivity";
+    ImageView pimg;
+    TextView ptitle, pprice, pdesc, pcat;
+    String pId, pImg, pTitle, pPrice, pCategory, pDescription;;
+    Button addToCart;
+    ImageButton btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+        ptitle = findViewById(R.id.txtProductTitle);
+        pcat = findViewById(R.id.txtProductCategory);
+        pprice = findViewById(R.id.txtProductPrice);
+        pdesc = findViewById(R.id.txtProductDesc);
+        pimg = findViewById(R.id.imgProduct);
+        btnBack = findViewById(R.id.btnBack);
+
+        if(getIntent().hasExtra("pid")){
+            pId = getIntent().getStringExtra("pid");
+            pTitle = getIntent().getStringExtra("ptitle");
+            pPrice = "Rs. " + getIntent().getStringExtra("pprice");
+            pImg = getIntent().getStringExtra("pimg");
+            pCategory = getIntent().getStringExtra("pcat");
+            pDescription = getIntent().getStringExtra("pdesc");
+
+            if(!Objects.equals(pImg , "")) {
+                Glide.with(this)
+                        .load(pImg)
+                        .into(pimg);
+            }else{
+                pimg.setBackgroundResource(R.drawable.no_image);
+            }
+            ptitle.setText(pTitle);
+            pcat.setText(pCategory);
+            pdesc.setText(pDescription);
+            pprice.setText(pPrice);
+        }else{
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        btnBack.setOnClickListener(v -> {
+            onBackPressed();
+        });
+    }
+
+}
--- app/src/main/java/com/example/ecommerce/ProductsRecyclerAdapter.java
+++ app/src/main/java/com/example/ecommerce/ProductsRecyclerAdapter.java
@@ -26,6 +26,7 @@ import java.util.Objects;
 
 
 public class ProductsRecyclerAdapter extends RecyclerView.Adapter<ProductsRecyclerAdapter.ViewHolder> {
+    private static final String TAG = "Adapter";
     Context context;
     String location;
     ArrayList<Products> arrProducts;
@@ -73,12 +74,37 @@ public class ProductsRecyclerAdapter extends RecyclerView.Adapter<ProductsRecycl
 //                SharedPreferences prefLocation = view.getContext().getSharedPreferences("location", MODE_PRIVATE);
 //                location = prefLocation.getString("city", "Select City");
 //                if(!Objects.equals(location, "Select City")){
-//                    Intent intent = new Intent(context, MovieDetailsActivity.class);
-//                    intent.putExtra("movie_name", arrSecond.get(holder.getLayoutPosition()).mName);
-//                    context.startActivity(intent);
+                    Log.d(TAG, "onClicked:");
+                    Intent intent = new Intent(context, ProductDetailsActivity.class);
+                    intent.putExtra("pid", arrProducts.get(holder.getLayoutPosition()).pId);
+                    intent.putExtra("pimg", arrProducts.get(holder.getLayoutPosition()).pImg);
+                    intent.putExtra("ptitle", title);
+                    intent.putExtra("pprice", arrProducts.get(holder.getLayoutPosition()).pPrice);
+                    intent.putExtra("pcat", arrProducts.get(holder.getLayoutPosition()).pCategory);
+                    intent.putExtra("pdesc", arrProducts.get(holder.getLayoutPosition()).pDescription);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                    context.getApplicationContext().startActivity(intent);
+                    Log.d(TAG, "intent passed");
 //                }else{
 //                    Toast.makeText(view.getContext(), "Please select a city", Toast.LENGTH_SHORT).show();
 //                }
+            }
+        });
+
+        holder.pimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClicked pimg:");
+                Intent intent = new Intent(context, ProductDetailsActivity.class);
+                intent.putExtra("pid", arrProducts.get(holder.getLayoutPosition()).pId);
+                intent.putExtra("pimg", arrProducts.get(holder.getLayoutPosition()).pImg);
+                intent.putExtra("ptitle", title);
+                intent.putExtra("pprice", arrProducts.get(holder.getLayoutPosition()).pPrice);
+                intent.putExtra("pcat", arrProducts.get(holder.getLayoutPosition()).pCategory);
+                intent.putExtra("pdesc", arrProducts.get(holder.getLayoutPosition()).pDescription);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                context.getApplicationContext().startActivity(intent);
+                Log.d(TAG, "pimg intent passed ");
             }
         });
     }
